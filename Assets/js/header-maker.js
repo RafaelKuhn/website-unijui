b79b843b8f8dc3080261aec4b8348c3e78654b2e
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2021-04-06 18:43:57 -0300

    feature: automatic header with javascript, page default formatting

---
+++ Assets/js/header-maker.js
@@ -0,0 +1,60 @@
+(function() { // iife
+const header = document.getElementById("header");
+if (!header) {
+  throw Error('here is no div with id "header" inside this file');
+}
+
+// navbar names and uris
+const listLinksJson = {
+  "Games": "./games.html",
+  "Quem somos": "./about.html",
+  "Contato": "./contact.html"
+}
+
+// css reference
+const headerCss = document.createElement("link");
+headerCss.rel = "stylesheet";
+headerCss.type = "text/css";
+headerCss.href = "../Assets/style/header.css"
+
+// logo
+const logoDiv = document.createElement("div");
+const logo = document.createElement("img");
+const logoLink = document.createElement("a");
+logoLink.href = "./index.html";
+logo.src = "../Assets/images/logo.png";
+
+logoDiv.classList.add("logo-div");
+
+// navigator menu
+const navbarList = document.createElement("ul");
+navbarList.classList.add("navbar");
+
+for (const linkRef in listLinksJson) {
+  const relativeLink = listLinksJson[linkRef];
+  
+  const genericListItem = document.createElement("li");
+  const genericLink = document.createElement("a");
+
+  genericLink.innerHTML = linkRef;
+  genericLink.href = relativeLink;
+
+  genericListItem.appendChild(genericLink);
+
+  navbarList.appendChild(genericListItem);
+}
+
+const navbarDiv = document.createElement("div");
+navbarDiv.classList.add("navbar-div");
+
+// appends
+document.head.appendChild(headerCss);
+
+logoLink.appendChild(logo)
+logoDiv.appendChild(logoLink);
+header.appendChild(logoDiv);
+
+navbarDiv.appendChild(navbarList);
+header.appendChild(navbarDiv);
+
+})();
