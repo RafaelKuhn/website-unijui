a2e4a9fa9fea4c549a6328c3ee582f98d5bd8e6b
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2021-06-03 19:27:55 -0300

    feature: header displays user if user is logged

--- Assets/html/header.php
+++ Assets/html/header.php
@@ -9,4 +9,38 @@
             <li><a href="<?php echo SERVER_ROOT_REQUEST ?>/pages/contact.php">Contato</a></li>
         </ul>
     </div>
+    <div class="user-area">
+        <?php
+        session_start();
+        $id = $_SESSION["id"] ?? null;
+        $username = $_SESSION["username"] ?? null;
+$id=1;$username="Kafael Ruhn";
+        if (is_null($id) || is_null($username)) {
+            echo
+            '<button class="dropbtn" onclick="location.href=\''.SERVER_ROOT_REQUEST.'/pages/user-entry.php\'">
+                <span>Login<br/>Registrar</span>
+            </button>';
+        } else {
+            $formatted_username = '';
+            if (strlen($username) > 15) {
+                $formatted_username = ucfirst(substr($username, 0, 13));
+                $formatted_username.="..";
+            } else {
+                $formatted_username = ucfirst($username);
+            }
+            ?>
+            <div class="dropdown">
+                <button class="dropbtn" onclick="location.href='<?php echo SERVER_ROOT_REQUEST ?>'">
+                    <span>Usuário:<br/><?php echo $formatted_username ?></span>
+                </button>
+                <div class="dropdown-content">
+                <h3><a href="<?php echo SERVER_ROOT_REQUEST ?>/pages/upload-game.php">⚔️ Subir game</a>
+                <h3><a href="<?php echo SERVER_ROOT_REQUEST ?>/pages/user-entry.php">❌ Sair</a>
+                </div>
+            </div>
+        <?php
+        }   ?>
+
+        
+    </div>
 </div>
